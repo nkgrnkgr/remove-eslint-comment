069f5f82af40e83028a918f48bb6868a5b05999a
nkgrnkgr <beefupme@gmail.com>
2024-09-28 13:20:41 +0900

    add

---
+++ parent/child/a1.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+const a = "";
+
+/* eslint-disable */
+const b = "";
--- script.ts
+++ script.ts
@@ -1,6 +1,27 @@
+import { readLines } from "https://deno.land/std@0.180.0/io/mod.ts";
+
 async function readIgnores() {
     const ignores = await Deno.readTextFile(".ignore");
     return ignores.split("\n").filter((line) => line.trim() !== "");
+}
+
+function shouldDeleteLine(line: string) {
+    return line.includes("eslint-disable");
+}
+
+async function deleteLinesFromFile(path: string) {
+    // 出力ファイルの内容を初期化
+    const output: string[] = [];
+
+    // ファイルを一行ずつ読み込む
+    const file = await Deno.open(path, { read: true });
+    for await (const line of readLines(file)) {
+        // 条件に応じて行をスキップ
+        if (!shouldDeleteLine(line)) {
+            output.push(line);
+        }
+    }
+    file.close();
 }
 
 async function readFilesRecursively(dir = ".", ignores: string[] = []) {
@@ -13,16 +34,13 @@ async function readFilesRecursively(dir = ".", ignores: string[] = []) {
         }
 
         if (entry.isFile) {
-            const data = await Deno.readTextFile(path);
-            console.log(path, data.length);
+            await deleteLinesFromFile(path);
         } else if (entry.isDirectory) {
-            console.log(path);
             await readFilesRecursively(path);
         }
     }
 }
 
 const ignores = await readIgnores();
-
 const targetDir = Deno.args[0];
 await readFilesRecursively(targetDir, ignores);
